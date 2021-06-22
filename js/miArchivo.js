05c1cc79659f43df6be2dcca77c6b1db3b466456
maribelalarcon <maribelsoledadalarcon@gmail.com>
2021-06-22 12:35:58 -0300

    Proyecto inicial sin javascript

---
+++ js/miArchivo.js
@@ -0,0 +1,17 @@
+// let ingresarProducto = true;
+// while(ingresarProducto){
+//     //usuario = prompt("Que desea comprar? /n A-TAZA /n B-VASOS")
+//     switch(usuario){
+//         case "A":
+//         case "a":
+//             alert("Elegiste TAZA--> Su precio es $500")
+//             break;
+//         case "B":
+//         case "b":
+//             alert("Elegiste VASO--> Su precio es $200")
+//             break;
+//             default:
+//                 alert("INGRESE UNA OPCION");
+//                 break; 
+//     }
+// }

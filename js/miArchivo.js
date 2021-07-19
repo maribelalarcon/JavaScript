1a86778f971070e49e0320b5e050ed2359d16817
maribelalarcon <maribelsoledadalarcon@gmail.com>
2021-07-19 17:15:40 -0300

    agregar jquery

--- js/miArchivo.js
+++ js/miArchivo.js
@@ -1,78 +1,60 @@
-// arreglo de objetos "producto", cada objeto
-// del arreglo va a tener todas las propiedades que necesitan
-// los productos: nombre, descripcion, precio, imagen, etc.
+$(document).ready(function(){
+    console.log("El DOM esta listo")
 
+});
+// arreglo de objetos "producto"
 const productos = [
     { id: "1", nombre: "Taza", precio: 200, imagen: "images/taza.jpeg"},
     { id: "2", nombre: "Plato", precio: 500, imagen: "images/platos.jpeg"},
     { id: "3", nombre: "Silla", precio: 1200, imagen: "images/silla.jpg"},
 ]
 
-// arreglo vacio que va a representar los productos
-// en el carrito
 let carrito = [];
 
-const elementoCantidadCarrito = document.getElementById("cantidad-carrito");
-
-const carritoJson = localStorage.getItem("carrito");
-
-if (carritoJson !== null) {
-    carrito = JSON.parse(carritoJson);
+//funcion para agregar la propiedad cantidad al objeto literal para el carriti
+function agregarItem(objeto){
+    console.log("Agregar objeto: ");
+    Object.defineProperty(objeto, "cant", {value:1, writable:true});
+    carrito.push(objeto);
 }
 
-const actualizarCarrito = () => {
-
-    let total = 0;
-
-    carrito.forEach(producto => {
-        total = total + producto.precio;
-    });
-
-    elementoCantidadCarrito.innerText = `${carrito.length} ($${total})`;
-
-    const carritoJson = JSON.stringify(carrito);
-    localStorage.setItem("carrito", carritoJson);
+//Funcion Actualizar Carrito
+function actualizarCarrito(){
+    console.log("carrito", carrito.length)
+    $("#cantidad-carrito").text(carrito.length);
 }
 
-//funcion que tome los datos del arreglo,
-// y los inserte en el DOM
+//Recorro producto
+for(const producto of productos) {
+    $("#productos").append(`<div class="col-sm-4">
+                                <img class="w-100" src="${producto.imagen}" />  
+                                <p> Producto: ${producto.nombre}</p>
+                                <b>$ ${producto.precio} </b>
+                                <button class="btn" id="${producto.id}"> Comprar </button> 
+                                <input type="number" min="1" value="1/>
+                            </div>`)}
+//definir funcion cuando ocurre evento
+$("#productos .btn").on('click',function(evento) {
+    console.log (`Compraste `,evento.target.id);
+    const idProducto = evento.target.id;
+    const producto = productos.find((p)=>{
+        return p.id === idProducto;
+    //  //find con funcion flecha y desestructuracion
+    // let objetoBuscado = carrito.find(({id) =>{
+    //     id == producto.id);
+    // //underfined si no lo encuentra
+    // console.log("El objeto:");
+    // console.log(objetoBuscado);
+    // })
+    console.log("producto ", producto);
+    //agregar producto al carrito
+    carrito.push(producto);
+    objetoBuscado();
+    actualizarCarrito();
 
-const contendorDeProductos = document.getElementById("productos");
 
-const actualizarProductos = () => {
-    for (let producto of productos) {
-        const elProducto = document.createElement("div");
-        elProducto.innerHTML = 
-            `<div class="col-md-4 text-center animate-box">
-                <div class="product">
-                    <div class="product-grid" style="background-position:center; background-image:url(${producto.imagen});">
-                        <div class="inner">
-                            <p>
-                                <a href="#" data-id="${producto.id}" class="icon boton-agregar-carrito"><i class="icon-shopping-cart"></i></a>
-                            </p>
-                        </div>
-                    </div>
 
-                    <div class="desc">
-                        <h3><a href="single.html">${producto.nombre}</a></h3>
-                        <span class="price">$${producto.precio}</span>
-                    </div>
-                </div>
-            </div>`;
-        contendorDeProductos.appendChild(elProducto);
-    }
-}
 
-actualizarProductos();
-actualizarCarrito();
 
-const botonesCarrito = document.getElementsByClassName("boton-agregar-carrito");
 
-for (let boton of botonesCarrito) {
-    boton.addEventListener("click", (event) => {
-        const id = event.currentTarget.getAttribute("data-id");
-        producto = productos.find((p) => p.id === id);
-        carrito.push(producto);
-        actualizarCarrito();
-    })
-}
+

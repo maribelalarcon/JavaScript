a4cfcc1c38ef73e9ce09f5d1feaac167df5d04de
maribelalarcon <maribelsoledadalarcon@gmail.com>
2021-08-03 09:03:31 -0300

    Arreglos de AJAX-Reentrega

--- js/miArchivo.js
+++ js/miArchivo.js
@@ -1,114 +1,102 @@
 $(document).ready(function(){
-    console.log("El DOM esta listo")
 
-});
-// arreglo de objetos "producto"
-const productos = [
-    { id: "1", nombre: "Taza", precio: 200, imagen: "images/taza.jpeg"},
-    { id: "2", nombre: "Plato", precio: 500, imagen: "images/platos.jpeg"},
-    { id: "3", nombre: "Silla", precio: 1200, imagen: "images/silla.jpg"},
-]
+    $("#boton-cargar-productos").click(() => {
 
-const carritoJson = localStorage.getItem("carrito");
-const carrito = carritoJson ? JSON.parse(carritoJson) : [];
-
-let cantidad = 0;
-carrito.forEach(p => cantidad += p.cantidad);
-
-$("#cantidad-carrito").text(cantidad);
-
-//funcion para agregar la propiedad cantidad al objeto literal para el carriti
-function agregarItem(objeto){
-    console.log("Agregar objeto: ");
-    Object.defineProperty(objeto, "cant", {value:1, writable:true});
-    carrito.push(objeto);
-}
-
-//Funcion Actualizar Carrito
-function actualizarCarrito(){
-    console.log("carrito", carrito.length)
-    const carritoJson = JSON.stringify(carrito);
-    localStorage.setItem("carrito", carritoJson);
-    let cantidad = 0;
-    carrito.forEach(producto => cantidad += producto.cantidad);
-    $("#cantidad-carrito").text(cantidad);
-}
-
-//Recorro producto
-for(const producto of productos) {
-    $("#productos").append(`<div class="col-sm-4">
-                                <img class="w-100" src="${producto.imagen}" />  
-                                <p> Producto: ${producto.nombre}</p>
-                                <b>$ ${producto.precio} </b>
-                                <button class="btn" id="${producto.id}"> Comprar </button> 
-                                <input type="number" min="1" value="1/>
-                            </div>`)}
-//definir funcion cuando ocurre evento
-$("#productos .btn").on('click',function(evento) {
-    console.log (`Compraste `,evento.target.id);
-
-    const idProducto = evento.target.id;
-    const producto = productos.find((p)=>{
-        return p.id === idProducto;
-    });
-
-    console.log("producto ", producto);
-
-    const productoCarrito = carrito.find((p)=>{
-        return p.id === idProducto;
-    });
-
-    if (productoCarrito) {
-        productoCarrito.cantidad = productoCarrito.cantidad + 1;
-    } else {
-        producto.cantidad = 1;
-        carrito.push(producto);
-    }
-
-    actualizarCarrito();
+        $.get("/productos.json", function(respuesta, estado) {
 
 
-    //agregar producto al carrito
-    $('.alerta').fadeIn();
-    setTimeout(() => $('.alerta').fadeOut(), 1000);
+            // agregar "producto" por json
+            const productos = respuesta;
+    
+            const carritoJson = localStorage.getItem("carrito");
+            const carrito = carritoJson ? JSON.parse(carritoJson) : [];
+        
+            let cantidad = 0;
+            carrito.forEach(p => cantidad += p.cantidad);
+        
+            $("#cantidad-carrito").text(cantidad);
+        
+            //funcion para agregar la propiedad cantidad al objeto literal para el carriti
+            function agregarItem(objeto){
+                console.log("Agregar objeto: ");
+                Object.defineProperty(objeto, "cant", {value:1, writable:true});
+                carrito.push(objeto);
+            }
+        
+            //Funcion Actualizar Carrito
+            function actualizarCarrito(){
+                console.log("carrito", carrito.length)
+                const carritoJson = JSON.stringify(carrito);
+                localStorage.setItem("carrito", carritoJson);
+                let cantidad = 0;
+                carrito.forEach(producto => cantidad += producto.cantidad);
+                $("#cantidad-carrito").text(cantidad);
+            }
+        
+            //Recorro producto
+            for(const producto of productos) {
+                $("#productos").append(`<div class="col-sm-4">
+                                            <img class="w-100" src="${producto.imagen}" />  
+                                            <p> Producto: ${producto.nombre}</p>
+                                            <b>$ ${producto.precio} </b>
+                                            <button class="btn" id="${producto.id}"> Comprar </button> 
+                                            <input type="number" min="1" value="1/>
+                                        </div>`)}
+            //definir funcion cuando ocurre evento
+            $("#productos .btn").on('click',function(evento) {
+                console.log (`Compraste `,evento.target.id);
+        
+                const idProducto = evento.target.id;
+                const producto = productos.find((p)=>{
+                    return p.id === idProducto;
+                });
+        
+                console.log("producto ", producto);
+        
+                const productoCarrito = carrito.find((p)=>{
+                    return p.id === idProducto;
+                });
+        
+                if (productoCarrito) {
+                    productoCarrito.cantidad = productoCarrito.cantidad + 1;
+                } else {
+                    producto.cantidad = 1;
+                    carrito.push(producto);
+                }
+        
+                actualizarCarrito();
+        
+        
+                //agregar producto al carrito
+                $('.alerta').fadeIn();
+                setTimeout(() => $('.alerta').fadeOut(), 1000);
+        
+            })
+        
+        
+            function suscribir() {
+                $("#alerta-suscripcion").removeClass("hide");
+                $("#email").val("");
+            }
+        
+        
+            // Suscripcion
+            $("#email").on("keypress", (evento) => {
+                if (evento.key === "Enter") {
+                    suscribir();
+                }
+            });
+        
+            $("#boton-suscribirse").on("click", () => {
+                suscribir();
+            });
+    
+    
+        })
 
-})
 
+    })
 
-function suscribir() {
-    $("#alerta-suscripcion").removeClass("hide");
-    $("#email").val("");
-}
-
-
-// Suscripcion
-$("#email").on("keypress", (evento) => {
-    if (evento.key === "Enter") {
-        suscribir();
-    }
-});
-
-$("#boton-suscribirse").on("click", () => {
-    suscribir();
-});
-
-//Declaramos la url que vamos a usar para el GET
-const URLGET = "https://jsonplaceholder.typicode.com/photos"
-//Agregamos un botón con jQuery
-$("#productos").prepend('<button id="btn1">GET</button>');
-//Escuchamos el evento click del botón agregado
-$("#btn1").click(() => { 
-    $.get(URLGET, function (respuesta, estado) {
-          if(estado === "success"){
-            let misDatos = respuesta;
-            for (const dato of misDatos) {
-              $("#productos").prepend(`<div>
-                                   <h3>${dato.url}</h3>
-                                   <p> ${dato.title}</p>
-                                  </div>`);
-            }  
-          }
-    });
 });
 
 
